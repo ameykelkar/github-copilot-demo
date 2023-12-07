25ca47ad6c4c0493582b60ff7e01935dbdf2a86c
ameykelkar <ameyk@live.com>
2023-12-07 12:18:22 -0800

    Add null analysis and build configuration settings, and update dependencies

--- my-spring-boot-app/src/main/java/com/example/myapp/controller/HelloWorldController.java
+++ my-spring-boot-app/src/main/java/com/example/myapp/controller/HelloWorldController.java
@@ -1,18 +1,31 @@
 package com.example.myapp.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
+    /**
+     * Retrieves a greeting message.
+     *
+     * @return ResponseEntity<Object> - The response entity containing the greeting
+     *         message as a JSON object.
+     */
     @GetMapping("/hello")
-    public String sayHello() {
-        return "Hello, World!";
+    public ResponseEntity<Object> sayHello() {
+        String message = "Hello, World!";
+        return ResponseEntity.status(HttpStatus.OK).body("{\"message\": \"" + message + "\"}");
     }
-    
+
     @GetMapping("/bye")
-        public String sayBye() {
-        return "Goodbye!";
+    public ResponseEntity<Object> sayBye() {
+        String message = "Goodbye!";
+        return ResponseEntity.status(HttpStatus.OK).body("{\"message\": \"" + message + "\"}");
     }
 }
--- my-spring-boot-app/src/main/java/com/example/myapp/controller/JwtController.java
+++ my-spring-boot-app/src/main/java/com/example/myapp/controller/JwtController.java
@@ -1,17 +1,25 @@
 package com.example.myapp.controller;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Key;
+import java.util.Date;
+
 @RestController
 public class JwtController {
+
+    private static final Key SECRET_KEY = Keys.secretKeyFor(io.jsonwebtoken.SignatureAlgorithm.HS256);
 
     @PostMapping("/validate")
     public String validateJwtToken(@RequestBody String jwtToken) {
         // TODO: Implement JWT token validation logic here
         // You can use libraries like jjwt or Nimbus JOSE + JWT for token validation
-        
+
         // BEGIN: Validation Logic
         if (isValidToken(jwtToken)) {
             return "Valid JWT token";
@@ -20,12 +28,24 @@ public class JwtController {
         }
         // END: Validation Logic
     }
-    
+
     private boolean isValidToken(String jwtToken) {
         // TODO: Implement JWT token validation logic here
         // You can use libraries like jjwt or Nimbus JOSE + JWT for token validation
-        
-        // Placeholder implementation
-        return jwtToken != null && !jwtToken.isEmpty();
+
+        try {
+            Claims claims = Jwts.parserBuilder()
+                    .setSigningKey(SECRET_KEY)
+                    .build()
+                    .parseClaimsJws(jwtToken)
+                    .getBody();
+
+            Date expirationDate = claims.getExpiration();
+            Date currentDate = new Date();
+
+            return !currentDate.after(expirationDate);
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
